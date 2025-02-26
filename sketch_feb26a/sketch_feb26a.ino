d242a596b40dbcdd245a80bc0bc7f5f2efbe50a4
John Nooyen Jr. <jpnooyen@gmail.com>
2025-02-26 09:53:12 -0500

    Create sketch_feb26a.ino
    It finally works again

---
+++ sketch_feb26a/sketch_feb26a.ino
@@ -0,0 +1,35 @@
+#include <arduinoFFT.h>
+const uint16_t samples = 128; //This value MUST ALWAYS be a power of 2
+const float signalFrequency = 1000;
+const float samplingFrequency = 2000;
+int samplingPeriod;
+const uint8_t amplitude = 100;
+
+float vReal[samples];
+float vImag[samples];
+
+ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, samples, samplingFrequency); /* Create FFT object */
+
+void setup(){
+    samplingPeriod = 1000000 / samplingFrequency;
+    Serial.begin(9600);
+}
+
+void loop() {
+    // Get samples
+    for (int i = 0; i < samples; i++)
+    {
+      vImag[i] = 0;
+      vReal[i] = analogRead(A0);
+      delayMicroseconds(samplingPeriod);
+      //Serial.println(vReal[i]);
+    }
+
+    FFT.dcRemoval(vReal, samples);
+    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	/* Weigh data */
+    FFT.compute(FFTDirection::Forward); /* Compute FFT */
+    FFT.complexToMagnitude(); /* Compute magnitudes */
+    Serial.println(FFT.majorPeak());
+    //delay(1000000);
+    // Rest of the code
+}
