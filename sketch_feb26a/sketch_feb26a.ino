6356f6370705c1541420d0c190a159b6f5b669c1
adamkhalek <adamabdulkhalek@gmail.com>
2025-03-21 09:35:16 -0400

    Update sketch_feb26a.ino
    Added LCD

--- sketch_feb26a/sketch_feb26a.ino
+++ sketch_feb26a/sketch_feb26a.ino
@@ -1,52 +1,121 @@
 #include <arduinoFFT.h>
 #include <math.h>
-const uint16_t samples = 128; //This value MUST ALWAYS be a power of 2
-//const float signalFrequency = 1000;
+#include <TimerOne.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Constants
+const uint16_t samples = 128;
 const float samplingFrequency = 1000;
 int samplingPeriod;
 const uint8_t amplitude = 100;
 
+// FFT Variables
 float vReal[samples];
 float vImag[samples];
 
+// Note Detection Variables
 float noteFreq;
 float noteConvert;
 int octave;
 int note;
+int cents;
+const char *noteline;
+const char *accuracyline;
 
+// I2C LCD Display
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
 
-ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, samples, samplingFrequency); /* Create FFT object */
+// FFT Object
+ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, samples, samplingFrequency);
 
-void setup(){
+void setup() {
     samplingPeriod = 1000000 / samplingFrequency - 125;
     Serial.begin(9600);
+    pinMode(9, OUTPUT);
+    Timer1.initialize(100);  // 100us = 10kHz
+    Timer1.pwm(9, 205);
+
+    // Initialize the LCD
+    lcd.init();
+    lcd.backlight(); // Turn on the backlight
+    lcd.setCursor(0, 0);
+    lcd.print("Tuner");
 }
 
 void loop() {
     // Get samples
-    for (int i = 0; i < samples; i++)
-    {
-      vImag[i] = 0;
-      vReal[i] = analogRead(A0);
-      delayMicroseconds(samplingPeriod);
+    for (int i = 0; i < samples; i++) {
+        vImag[i] = 0;
+        vReal[i] = analogRead(A0);
+        delayMicroseconds(samplingPeriod);
     }
 
+    // Perform FFT
     FFT.dcRemoval(vReal, samples);
-    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	/* Weigh data */
-    FFT.compute(FFTDirection::Forward); /* Compute FFT */
-    FFT.complexToMagnitude(); /* Compute magnitudes */
+    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);  // Weigh data
+    FFT.compute(FFTDirection::Forward);  // Compute FFT
+    FFT.complexToMagnitude();  // Compute magnitudes
     noteFreq = FFT.majorPeak();
-    noteConvert = (12*log10(noteFreq/27.5)/log10(2))+1;
-    Serial.println(noteConvert);
-    octave = (round(noteConvert) - 1)/12;
-    note = round(noteConvert - 12*octave);
-    Serial.println(note);
-    if(note > 2) {
-      octave++;  }
-    Serial.println(octave);
 
+    // Convert frequency to note
+    noteConvert = (12 * log10(noteFreq / 27.5) / log10(2)) + 1;
+    octave = (round(noteConvert) - 1) / 12;
+    note = round(noteConvert - 12 * octave);
+    cents = round((noteConvert - round(noteConvert)) * 10);
+
+    if (note > 3) {
+        octave++;
+    }
+
+    // Determine note name
+    switch (note) {
+        case 1: noteline = "A"; break;
+        case 2: noteline = "A#/Bb"; break;
+        case 3: noteline = "B"; break;
+        case 4: noteline = "C"; break;
+        case 5: noteline = "C#/Db"; break;
+        case 6: noteline = "D"; break;
+        case 7: noteline = "D#/Eb"; break;
+        case 8: noteline = "E"; break;
+        case 9: noteline = "F"; break;
+        case 10: noteline = "F#/Gb"; break;
+        case 11: noteline = "G"; break;
+        case 12: noteline = "G#/Ab"; break;
+    }
+
+    // Determine accuracy
+    switch (cents) {
+        case -5: accuracyline = "||||| =="; break;
+        case -4: accuracyline = "|||| =="; break;
+        case -3: accuracyline = "||| =="; break;
+        case -2: accuracyline = "|| =="; break;
+        case -1: accuracyline = "| =="; break;
+        case 0: accuracyline = "=="; break;
+        case 1: accuracyline = "== |"; break;
+        case 2: accuracyline = "== ||"; break;
+        case 3: accuracyline = "== |||"; break;
+        case 4: accuracyline = "== ||||"; break;
+        case 5: accuracyline = "== |||||"; break;
+    }
+
+    // Display on LCD
+    lcd.setCursor(0, 0);
+    lcd.print("Note: ");
+    lcd.print(noteline);
+    lcd.print(" ");
+    lcd.print(octave);
+    lcd.print("      "); // Clear the rest of the line with spaces
+
+    lcd.setCursor(0, 1);
+    lcd.print("Accuracy: ");
+    lcd.print(accuracyline);
+    lcd.print("      "); // Clear the rest of the line with spaces
+
+    // Debugging output to Serial Monitor
+    Serial.println("================");
+    Serial.println(noteline);
+    Serial.println(accuracyline);
+    Serial.println("================");
     Serial.println(FFT.majorPeak());
-    // Rest of the code
 }
-
-
